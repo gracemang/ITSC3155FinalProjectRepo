ba93a37fec2024bc8f47015cea038dfd31560db1
John Gambino <jgambin1@uncc.edu>
2020-04-08 17:43:06 +0000

    Attempted to do route and failed

--- blog/app/controllers/books_controller.rb
+++ blog/app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
+    #def show
+        #@books = Book.find(params[:isbnnumber])
+    #end
+    
     def new
     end
     
     def create
-        render plain: params[:isbn].inspect
+        #render plain: params[:isbn].inspect
+        @books = Book.new(book_params)
+        @books.save
+        redirect_to @books
+    end
+    
+private 
+    def book_params
+        params.require(:books).permit(:isbn, nil)
     end
 end
